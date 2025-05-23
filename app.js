43a43b5af76de781d95ff14132c5362d1035e514
CyberTovar <tovarhurtadosalome1@gmail.com>
2025-05-23 07:47:31 -0500

    modificaciones

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const { WebhookClient } = require('dialogflow-fulfillment');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 
@@ -21,6 +22,26 @@ app.get('/', function (req, res){
 });
 
 app.post('/webhook', (req, res) => {
+
+
+  const agent = new WebhookClient({ request: req, response: res });
+
+    function welcome(agent) {
+        agent.add('Welcome to my agent!');
+    }
+
+    function fallback(agent) {
+        agent.add('I did not understand');
+        agent.add('I am sorry, can you try again?');
+    }
+
+    let intentMap = new Map();
+
+    intentMap.set('Default Welcome Intent', welcome);
+    intentMap.set('Default Fallback Intent', fallback);
+    
+    agent.handleRequest(intentMap);
+
   console.log("Peticion del Webhook en DialogFlow", req.body);
   var speech =
   req.body.queryResult &&
