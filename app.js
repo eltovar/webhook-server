38e61be3008c275ef55f21ae74e68da82607969e
CyberTovar <tovarhurtadosalome1@gmail.com>
2025-05-23 08:41:19 -0500

    feat: Handle Default Welcome Intent via webhook

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ app.get('/', function (req, res){
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.post('/webhook', (req, res) => {
+
+app.post('/webhook', (req, res) => { //ruta del webhook Inicio
 
 
   const agent = new WebhookClient({ request: req, response: res });
@@ -29,21 +30,24 @@ app.post('/webhook', (req, res) => {
     function welcome(agent) {
         agent.add('Welcome to my agent!');
     }
-
+  
+    // --- Función para manejar el "Default Welcome Intent" ---
     function fallback(agent) {
-        agent.add('I did not understand');
-        agent.add('I am sorry, can you try again?');
+      console.log("Intent Default Welcome Intent activado.");
+      agent.add('Hola!!, soy un bot de prueba');
     }
 
+    // --- Mapeo de Intents a funciones manejadoras ---
     let intentMap = new Map();
-
-    intentMap.set('Default Welcome Intent', welcome);
-    intentMap.set('Default Fallback Intent', fallback);
+    intentMap.set('Default Welcome Intent', welcome);// Mapea el Intent de Dialogflow a tu función 'welcome'
+    intentMap.set('Default Fallback Intent', fallback); // Mapea el Intent de Dialogflow a tu función 'fallback'
+    
     
     agent.handleRequest(intentMap);
 
   console.log("Peticion del Webhook en DialogFlow", req.body);
-  var speech =
+  
+  /*var speech =
   req.body.queryResult &&
   req.body.queryResult.parameters &&
   req.body.queryResult.parameters.echoText
@@ -67,7 +71,9 @@ app.post('/webhook', (req, res) => {
     //Logica para procesar el webhook de DialogFlow
 
   });
+*/
 
+  });//fin del webhook
 
 app.listen(port, () => {
     // Imprime un mensaje en la consola indicando que el servidor está corriendo y la dirección en la que se puede acceder.
