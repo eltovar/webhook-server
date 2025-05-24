efd63d958376408897e19394f60fb3cd8fb8a2c2
CyberTovar <tovarhurtadosalome1@gmail.com>
2025-05-24 15:56:48 -0500

    prueba decirHola1.4 otra vez con el else

--- app.js
+++ app.js
@@ -57,7 +57,9 @@ app.post('/webhook', express.json(), function (req, res){  //ruta del webhook In
       const person = String(agent.parameters.person); // Convierte a cadena explícitamente
         if (person && person !== 'undefined' && person !== 'null' && person !== '[object Object]') { // Filtra valores no deseados
          agent.add(`¡Hola, ${person}! Es un placer saludarte desde el webhook.`);
-        } 
+        } else {
+          agent.add('¡Hola! Es un placer saludarte desde el webhook.');
+        }
       } 
     
 
